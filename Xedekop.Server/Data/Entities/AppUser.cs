32d369bc2346ef96e896a37fc6c0cf58e162c157
BrandonBoros <2377215@johnabbottcollege.net>
2025-11-04 12:24:36 -0500

    Created Models, Database, and started security.

---
+++ Xedekop.Server/Data/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Xedekop.Server.Data.Entities
+{
+    /// <summary>
+    /// User of the app.
+    /// </summary>
+    public class AppUser : IdentityUser<int> { }
+}
