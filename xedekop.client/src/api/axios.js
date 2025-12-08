a75a63682398d19c42bec4c7b7287454e07e47e9
Brandon <158082312+BrandonBoros@users.noreply.github.com>
2025-12-08 08:50:04 -0500

    Update axios.js

--- xedekop.client/src/api/axios.js
+++ xedekop.client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7135/api",
+    baseURL: "https://xedekop-e4fwgmbjage2gabv.eastus2-01.azurewebsites.net/api",
     headers: {
         "Content-Type": "application/json"
     }
